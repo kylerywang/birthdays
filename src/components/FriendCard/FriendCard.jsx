cb7a309ecc182f730bccfdc04e7c2adbd90d0fd8
kylerwan <kyler.y.wang@gmail.com>
2023-03-13 18:06:59 -0700

    reimplement delete button

--- src/components/FriendCard/FriendCard.jsx
+++ src/components/FriendCard/FriendCard.jsx
@@ -3,9 +3,6 @@ export default function FriendCard({friend, deleteFriend}) {
         deleteFriend(friend)
     }
 
-    // async function sendText(){
-    //     editFriend(friend)
-    // }
 
     return (
         <div className="flex items-center justify-between bg-white rounded-lg px-6 py-4">
@@ -13,7 +10,7 @@ export default function FriendCard({friend, deleteFriend}) {
                 <h3 className="text-lg font-medium text-gray-900">{friend.name}</h3>
                 <p className="text-sm font-medium text-gray-500 mt-1">Birthday: {friend.birthday.month}/{friend.birthday.day}</p>
             </div>
-            <button className="flex-shrink-0 bg-blue-500 text-white font-medium py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            <button className="flex-shrink-0 bg-blue-500 text-white font-medium py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" onClick={handleDeleteButton}>
                 Delete
             </button>
         </div>
